b6d37d21b39389c8ffa4ec518e91d858831eca97
Binhui Chen Zhou <779799@unizar.es>
2021-12-14 08:35:17 +0100

    max:n supported

--- src/main/kotlin/soa/camel/Application.kt
+++ src/main/kotlin/soa/camel/Application.kt
@@ -43,6 +43,14 @@ class Router(meterRegistry: MeterRegistry) : RouteBuilder() {
 
     override fun configure() {
         from(DIRECT_ROUTE)
+            .process { exchange ->
+                var keyword = exchange.getIn().getHeader("keywords")
+                if (keyword is String && keyword.contains("max:[0-9]+".toRegex())) {
+                    keyword = keyword.replace(
+                        Regex("(.*)(max:)([0-9]+ ?)$"), "$1?count=$3")
+                }
+                exchange.getIn().setHeader("keywords", keyword)
+            }
             .toD("twitter-search:\${header.keywords}")
             .wireTap(LOG_ROUTE)
             .wireTap(COUNT_ROUTE)
